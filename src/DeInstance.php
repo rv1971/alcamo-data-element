2a12b8ef5915cc36c558ae09c772d36546ab4da8
rv1971 <rv1971@web.de>
2026-05-14 11:44:44 +0200

    Add new files that should have been added before

---
+++ src/DeInstance.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace alcamo\data_element;
+
+use alcamo\rdf_literal\LiteralInterface;
+
+/**
+ * @brief Data element instance
+ *
+ * @date Last reviewed 2026-05-05
+ */
+class DeInstance implements DeInstanceInterface
+{
+    private $dataElement_; ///< DataElementInterface
+    private $literal_;     ///< LiteralInterface
+
+    public function __construct(
+        DataElementInterface $dataElement,
+        LiteralInterface $literal
+    ) {
+        $this->dataElement_ = $dataElement;
+        $this->literal_ = $literal;
+    }
+
+    public function getDataElement(): DataElementInterface
+    {
+        return $this->dataElement_;
+    }
+
+    public function getLiteral(): LiteralInterface
+    {
+        return $this->literal_;
+    }
+}
---
+++ src/DeInstanceInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace alcamo\data_element;
+
+use alcamo\rdf_literal\LiteralInterface;
+
+/**
+ * @brief Instance of a data element with a literal as its value
+ *
+ * @date Last reviewed 2026-05-04
+ */
+interface DeInstanceInterface
+{
+    public function getDataElement(): DataElementInterface;
+
+    public function getLiteral(): LiteralInterface;
+}
